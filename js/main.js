e64221704e966db9e011d9982a18208179096f38
ghassul <ghassul@gmail.com>
2015-10-13 13:48:25 -0700

    Adding lightbox

--- js/main.js
+++ js/main.js
@@ -1,47 +1,93 @@
 $(function () {
 
-    $('.instagram-searcher').on('submit', function(event) {
+    var pagination_link = '';
+    var $imageList = $('<ul class="image-list"></ul>');
+    var i = 0;
 
-        event.preventDefault();
-        var formInput = $('input').val();
-        var searchInput = "https://api.instagram.com/v1/tags/" + formInput
-            + "/media/recent?client_id=e7c23d1d42974762a329e5dfc09ff86f";
-        var i = 0;
-
-            $('.image-list').empty();
+    $('.loading_gif').toggle(false);
 
 
+    function imgagePopulator(imgInput) {
+
+
+//<a href="images/image-1.jpg" data-lightbox="image-1" data-title="My caption">Image #1</a>
 
         $.ajax({
-            method: 'GET',
-            dataType: 'jsonp',
-            url: searchInput
-        })
-
-            .done(function(data) {
-                console.log(data);
-
-                $.each(data.data, function(key, value) {
-                    //console.log(value);
-                    i++;
-                    if(data.data !== 0 && i < 13) {
-                        console.log(value.images.thumbnail.url);
-                        $('.image-list').append("<li><img src=\'" + value.images.thumbnail.url + "\'></li>")
-
-
-                    }
-                })
+                method: 'GET',
+                dataType: 'jsonp',
+                url: imgInput
             })
 
+            .done(function (data) {
+                console.log(data);
+
+                pagination_link = (data.pagination.next_url);
+
+                $.each(data.data, function (key, value) {
+                    i++;
+
+                    var listItems = '';
+                    listItems += "<li><img src=\'";
+                    listItems += value.images.standard_resolution.url + "\' class='content_image'>";
+                    listItems += "<div class='pic_footer'>" + "<img src=\'" + value.user.profile_picture + "\' class='profile_picture'>";
+                    listItems += "<div class='pic_info'><span class='user_name'>" + value.user.username + "</span>";
+                    listItems += "<div class='comment_likes'>";
+                    listItems += "<span class='comments'><i class='fa fa-comments'></i>" + value.comments.count + "</span>";
+                    listItems += "<span><i class='fa fa-heart'></i>" + value.likes.count + "</span></div></div>";
+
+                    listItems += "</div></li>";
+
+
+                    $imageList.append(listItems);
+
+
+                });
+
+                $('.imageListWrapper').append($imageList);
+                $('.loading_gif').toggle(false);
+                $('.load_more_btn').append("<button type=submit class='load_more'>Load more</button>");
+
+
+            });
+
+
+    }
 
 
 
 
-    })
+
+    $('.instagram-searcher').on('submit', function(event) {
+        event.preventDefault();
+        $('.imageListWrapper').empty();
+        $('.load_more_btn').empty();
+        $('.loading_gif').toggle(true);
+        $imageList.empty();
+
+        var formInput = $('input').val();
+        var searchInput = "https://api.instagram.com/v1/tags/" + formInput
+            + "/media/recent?count=12&client_id=e7c23d1d42974762a329e5dfc09ff86f";
+
+        imgagePopulator(searchInput);
+
+            });
 
 
 
 
+    $('.load_more_btn').on('click', function(event) {
+        event.preventDefault();
+        $('.load_more_btn').empty();
+        $('.loading_gif').toggle(true);
+        console.log(pagination_link);
+
+        imgagePopulator(pagination_link);
+    });
+
+
+
+
+9
 
 
 
